4fe28e1b0a8a7d47142ded047a4d6b4bcad7b8cf
Henning Post <henningpost@gmail.com>
2019-10-30 16:20:15 -0700

    Add unit tests

--- src/test/kotlin/cc/femto/rx/extensions/MappingSpec.kt
+++ src/test/kotlin/cc/femto/rx/extensions/MappingSpec.kt
@@ -1,7 +1,119 @@
 package cc.femto.rx.extensions
 
+import com.gojuno.koptional.None
+import com.gojuno.koptional.Optional
+import com.gojuno.koptional.Some
+import io.reactivex.Observable
+import io.reactivex.observers.TestObserver
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 
 class MappingSpec : Spek({
+
+    data class State(
+        val foo: String = "foo",
+        val bar: String? = null
+    )
+
+    lateinit var state: Observable<State>
+
+    describe("mapDistinct") {
+        lateinit var observer: TestObserver<String>
+
+        beforeEachTest {
+            state = Observable.just(
+                State(),
+                State(bar = "initialized"),
+                State(foo = "changed")
+            )
+            observer = state.mapDistinct { foo }.test()
+        }
+
+        it("maps to property and makes emissions distinct") {
+            observer.assertValues(
+                "foo",
+                "changed"
+            )
+        }
+    }
+
+    describe("mapOptional") {
+        lateinit var observer: TestObserver<Optional<String>>
+
+        beforeEachTest {
+            state = Observable.just(
+                State(),
+                State(bar = "initialized"),
+                State(foo = "changed")
+            )
+            observer = state.mapOptional { bar }.test()
+        }
+
+        it("maps to nullable property and wraps in `Optional`") {
+            observer.assertValues(
+                None,
+                Some("initialized"),
+                None
+            )
+        }
+    }
+
+    describe("mapSome") {
+        lateinit var observer: TestObserver<String>
+
+        beforeEachTest {
+            state = Observable.just(
+                State(),
+                State(bar = "initialized"),
+                State(foo = "changed", bar = "initialized")
+            )
+            observer = state.mapSome { bar }.test()
+        }
+
+        it("maps to nullable property, wraps in `Optional` and filters for `Some`") {
+            observer.assertValues(
+                "initialized",
+                "initialized"
+            )
+        }
+    }
+
+    describe("mapSomeDistinct") {
+        lateinit var observer: TestObserver<String>
+
+        beforeEachTest {
+            state = Observable.just(
+                State(),
+                State(bar = "initialized"),
+                State(foo = "changed", bar = "initialized")
+            )
+            observer = state.mapSomeDistinct { bar }.test()
+        }
+
+        it("maps to nullable property, wraps in `Optional` and filters for distinct `Some`") {
+            observer.assertValues(
+                "initialized"
+            )
+        }
+    }
+
+    describe("mapSomeOnce") {
+        lateinit var observer: TestObserver<String>
+
+        beforeEachTest {
+            state = Observable.just(
+                State(),
+                State(bar = "initialized"),
+                State(foo = "changed", bar = "initialized"),
+                State(bar = "changed")
+            )
+            observer = state.mapSomeOnce { bar }.test()
+        }
+
+        it("maps to nullable property, wraps in `Optional`, filters for `Some` and unsubscribes after one emission") {
+            observer.assertValues(
+                "initialized"
+            )
+        }
+    }
 })
